0f835627c963c1fcd56d4161d6f531f97ca3674a
Katarzyna Kaczmarek <katarzynakaczmarek392@gmail.com>
2022-12-05 12:18:24 +0100

    homework

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,12 +10,28 @@ public class SeleniumTest {
 
     @Test
     public void openGooglePage() {
-       System.setProperty("webdriver.chrome.drive","C:\\Users\\kaczm\\Downloads\\chromedriver_win32\\chromedriver.exe");
-       System.setProperty("webdriver.gecko.driver","C:\\Users\\kaczm\\Downloads\\geckodriver-v0.32.0-win-aarch64\\geckodriver.exe");
-       System.setProperty("webdriver.ie.driver","C:\\Users\\kaczm\\Downloads\\IEDriverServer_x64_4.6.0\\IEDriverServer.exe");
-        InternetExplorerOptions options = new InternetExplorerOptions();
-        options.withInitialBrowserUrl("https://www.google.com");
-        WebDriver driver=new InternetExplorerDriver(options);
 
+        WebDriver driver= getDriver("asc");
+
+           }
+           public WebDriver getDriver(String browser){
+                switch (browser) {
+                    case "chrome":
+                        String chromePath = "C:\\Users\\kaczm\\Downloads\\chromedriver_win32\\chromedriver.exe";
+                        System.setProperty("webdriver.chrome.drive",chromePath);
+                        return new ChromeDriver();
+                    case "firefox":
+                        String firefoxPath= "C:\\Users\\kaczm\\Downloads\\geckodriver-v0.32.0-win-aarch64\\geckodriver.exe";
+                        System.setProperty("webdriver.gecko.driver",firefoxPath);
+                        return new FirefoxDriver();
+                    case "ie":
+                        String iePath = "C:\\Users\\kaczm\\Downloads\\IEDriverServer_x64_4.6.0\\IEDriverServer.exe";
+                        System.setProperty("webdriver.ie.driver",iePath );
+                        return new InternetExplorerDriver();
+                    default:
+                        throw new InvalidArgumentException("Invalid browser name");
+
+
+                }
            }
 }
