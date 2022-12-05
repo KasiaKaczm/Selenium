fd6011679e191d5a26c4482fdf1113c700d9f65f
Katarzyna Kaczmarek <katarzynakaczmarek392@gmail.com>
2022-12-05 11:57:23 +0100

    SeleniumTest (exercises , opening browsers)

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,15 +1,20 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
 
-    public static void main(String[] args) {
-        WebDriver driver = new FirefoxDriver();
-    }
-
     @Test
-    public void sampleTest() {
-        System.out.println("Hello");
-    }
+    public void openGooglePage() {
+       System.setProperty("webdriver.chrome.drive","C:\\Users\\kaczm\\Downloads\\chromedriver_win32\\chromedriver.exe");
+       System.setProperty("webdriver.gecko.driver","C:\\Users\\kaczm\\Downloads\\geckodriver-v0.32.0-win-aarch64\\geckodriver.exe");
+       System.setProperty("webdriver.ie.driver","C:\\Users\\kaczm\\Downloads\\IEDriverServer_x64_4.6.0\\IEDriverServer.exe");
+        InternetExplorerOptions options = new InternetExplorerOptions();
+        options.withInitialBrowserUrl("https://www.google.com");
+        WebDriver driver=new InternetExplorerDriver(options);
+
+           }
 }
