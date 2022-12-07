339689eba6a1ede4b7ae5cf0e2ef42f82a707029
Katarzyna Kaczmarek <katarzynakaczmarek392@gmail.com>
2022-12-07 11:55:45 +0100

    WebDriverManager/first selenium test etc (lessons from 150 to 157)

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,9 +1,9 @@
-import org.openqa.selenium.InvalidArgumentException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
@@ -11,14 +11,29 @@ public class SeleniumTest {
     @Test
     public void openGooglePage() {
 
-        WebDriver driver= getDriver("asc");
+        WebDriver driver= getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
 
-           }
+        //znajdujemy przycisk
+        WebElement agreeButton=driver.findElement(By.xpath("//div[text()='Zaakceptuj wszystko']"));
+        //klikniecie przycisku
+        agreeButton.click();
+        //znajdz pole wyszukiwania
+        WebElement searchField = driver.findElement(By.name("q"));
+       //wprowadz wartosc Selenium do pola
+        searchField.sendKeys("Selenium");
+        //zasymuluj nacisniecie przycisku Enter
+        searchField.sendKeys(Keys.ENTER);
+        //znalezc rezultat
+        WebElement result = driver.findElement(By.xpath("//a[contains(@href,'selenium.dev')]//h3"));
+
+        Assert.assertTrue(result.isDisplayed());
+    }
+
            public WebDriver getDriver(String browser){
                 switch (browser) {
                     case "chrome":
-                        String chromePath = "C:\\Users\\kaczm\\Downloads\\chromedriver_win32\\chromedriver.exe";
-                        System.setProperty("webdriver.chrome.drive",chromePath);
                         return new ChromeDriver();
                     case "firefox":
                         String firefoxPath= "C:\\Users\\kaczm\\Downloads\\geckodriver-v0.32.0-win-aarch64\\geckodriver.exe";
